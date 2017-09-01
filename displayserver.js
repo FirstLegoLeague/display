1fe7a4d43b36de682324cc45fc0f08b9fadd7475
ThinkRedstone <ittai11@gmail.com>
2017-09-02 00:50:52 +0300

    added config override option

--- displayserver.js
+++ displayserver.js
@@ -1,6 +1,18 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var port = 1391;
-connect().use(serveStatic(__dirname)).listen(port);
+var argv = require('minimist')(process.argv.slice(2));
+var path = require('path');
+var send = require('send');
+
+var port = argv.p || 1391;
+var configFile = argv.config ? path.resolve(argv.config) : path.resolve(path.dirname(process.argv[1]), 'config.js');
+
+
+var app = connect();
+app.use('/config.js', function (req, res) {
+    send(req, configFile).pipe(res);
+});
+
+app.use(serveStatic(__dirname)).listen(port);
 console.log('server running on port',port);
 console.log('open browser to http://localhost:'+port+'/');
