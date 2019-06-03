07150ee38d38f0d0b5633f2e356c9c02e8595420
eshels <eshel.sinclair@gmail.com>
2019-06-03 20:40:11 +0300

    added default value

--- src/js/components/controls/NumericSlider.jsx
+++ src/js/components/controls/NumericSlider.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import '../../../../scss/components/NumericSlider.scss'
+import '../../../scss/components/NumericSlider.scss'
 
 class NumericSlider extends Component{
     constructor(props){
@@ -9,7 +9,7 @@ class NumericSlider extends Component{
 
     render() {
         return [<input id={this.props.setting.key} name={this.props.setting.key}
-            type="range" min="10" max="10000" defaultValue="10" className="slider"
+            type="range" min="10" max="10000" defaultValue={this.props.setting.value} className="slider"
             onChange={this.props.onUpdate}/>]
     }
 }
