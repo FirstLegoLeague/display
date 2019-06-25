9c2a42b6413ee64f3c2ddc9a300e68db8fec5189
eshels <eshel.sinclair@gmail.com>
2019-06-25 21:57:10 +0300

    added min, max to props

--- src/js/components/controls/NumericSlider.jsx
+++ src/js/components/controls/NumericSlider.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import '../../../scss/components/NumericSlider.scss'
 
+const MIN = "10"
+const MAX = "1000"
+
 class NumericSlider extends Component{
     constructor(props){
         super(props);
@@ -9,7 +12,7 @@ class NumericSlider extends Component{
 
     render() {
         return <input id={this.props.setting.key} name={this.props.setting.key}
-            type="range" min="10" max="1000" defaultValue={this.props.setting.value} className="slider"
+            type="range" min={this.props.min || MIN} max={this.props.max || MAX} defaultValue={this.props.setting.value} className="slider"
             onChange={this.props.onUpdate}/>
     }
 }
