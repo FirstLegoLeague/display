4642125cdd296a3d578c7cbe48b834aced51d07f
LPNG1 <itamar.oryan@gmail.com>
2019-09-14 22:01:31 +0300

    Added a setting for button sizes

---
+++ src/js/components/controls/ButtonSet.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { ButtonGroup, Button } from 'semantic-ui-react'
+
+class ButtonSet extends Component {
+  constructor (props) {
+    super(props)
+    this.props = props
+  }
+
+  render () {
+    return <ButtonGroup id={this.props.setting.key} name={this.props.setting.key}>
+      {this.props.buttonValues.map(value => { return <Button value={value} onClick={this.props.onClick}>{value}</Button> })}
+    </ButtonGroup>
+  }
+}
+
+export default ButtonSet
