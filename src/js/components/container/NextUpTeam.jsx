9a27ad590a013ce3bf4c4ada2eeb10dbf858658e
Idan Stark <idanstark42@gmail.com>
2020-03-04 20:30:46 +0200

    last change to layout

--- src/js/components/container/NextUpTeam.jsx
+++ src/js/components/container/NextUpTeam.jsx
@@ -1,17 +1,6 @@
 import React, { Component } from 'react'
 import { Textfit } from 'react-textfit'
 
-// import Settings from '../../services/settings'
-
-// const FONT_SIZES = {
-//   Tiny: { SMALLER: '12xp', LARGER: '20px' },
-//   Small: { SMALLER: '16px', LARGER: '24px' },
-//   Medium: { SMALLER: '20px', LARGER: '28px' },
-//   Large: { SMALLER: '24px', LARGER: '32px' },
-//   Giant: { SMALLER: '28px', LARGER: '36px' },
-//   Huge: { SMALLER: '32px', LARGER: '40px' }
-// }
-
 class NextUpTeam extends Component {
   render () {
     const tableName = this.props.table.tableName
@@ -19,13 +8,13 @@ class NextUpTeam extends Component {
     return <div className='column' style={{ width: `${100 / this.props.columns}%` }}>
       <div className='ui raised segment'>
         <div className='ui grid'>
-          <div className='five wide column'>
+          <div className='four wide column'>
             <Textfit className='ui header' mode='single' max='70' forceSingleModeWidth={false}>
               <div className='sub header'>{tableName}</div>
               #{team.number}
             </Textfit>
           </div>
-          <div className='eleven wide column'>
+          <div className='twelve wide column'>
             <Textfit className='ui header' mode='single' max='70' forceSingleModeWidth={false}>
               {team.name}
               <div className='sub header'>{team.affiliation}</div>
